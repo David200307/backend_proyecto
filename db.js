f40687c323d50d94cb16fddd779b7576d2ca14c9
David200307 <juandavidfernandeztriana@gmail.com>
2024-12-08 18:53:19 -0500

    database atlas

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
+
 export const connectDB = async () => {
     try {
-        await mongoose.connect ('mongodb://localhost/merndb')
-        console.log("dbconect")
+        await mongoose.connect('mongodb+srv://david200306:yqp80D7v1EA3jYGJ@proyectosena.0erwo.mongodb.net/?retryWrites=true&w=majority&appName=proyectosena');
+        console.log("Conexión exitosa a la base de datos");
     } catch (error) {
-        console.log("Error");
+        console.error("Error al conectar con la base de datos:", error.message);
     }
-}
+};
